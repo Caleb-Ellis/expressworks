0e3010c036fb8866f67c58ec8bfc07a41dd4ef9a
CEllis <tehwavy@gmail.com>
2017-05-20 14:53:26 +1000

    added ex05 and ex06 - added stylus module

--- ex05.js
+++ ex05.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
 var filepath = process.argv[3];
 var port = Number(process.argv[2]);
 
-app.use(express.static(filepath||path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(filepath));
+app.use(express.static(filepath));
 
 app.listen(port)
